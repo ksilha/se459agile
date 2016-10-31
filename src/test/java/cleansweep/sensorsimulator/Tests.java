a624a5cc274c5f76f5bbc6bd51e228e6d43e1be1
Kevin Krewer <kevrk555@gmail.com>
2016-10-30 20:37:35 -0500

    Added tests for vacuum, Added exception to clean for when the vacuum is full

--- src/test/java/cleansweep/sensorsimulator/Tests.java
+++ src/test/java/cleansweep/sensorsimulator/Tests.java
@@ -389,6 +389,63 @@ public class Tests {
 		assertNotNull("Error, VacuumSystem v was not created", v);
 	}
 	
+	@Test
+	public void testClean() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		int dirt = v.getTotalDirtWeight();
+		v.clean();
+		assertTrue("There was an error: starting dirt weight was " + dirt + " current dirt weight is " + v.getTotalDirtWeight(),v.getTotalDirtWeight() > dirt);
+	}
+	
+	@Test
+	public void testIsFull() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		int i = 0;
+		while (!v.isFull()) {
+			v.clean();
+			i++;
+		}
+		assertEquals(v.getTotalDirtWeight(), 50);
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		assertTrue(v.isEmpty());
+
+		
+	}
+	
+	@Test(expected=Exception.class)
+	public void testOverCapacity() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		int i = 0;
+		while (i < 100) {
+			v.clean();
+			i++;
+		}
+	}
+	
+	@Test
+	public void testEmptyDirt() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		int i = 0;
+		while (!v.isFull()) {
+			v.clean();
+			i++;
+		}
+		int currentDirt = v.getTotalDirtWeight();
+		v.emptyDirtBasket();
+		assertTrue("Error - After emptying, dirt weight is " + v.getTotalDirtWeight(),currentDirt == 50 && v.getTotalDirtWeight() == 0);
+	}
+	
+	@Test
+	public void testGetDirtCapacity() {
+		VacuumSystem v = VacuumSystemFactory.createVacuum();
+		assertEquals("Dirt capacity is " + v.getCapacity() + " but should be 50", v.getCapacity(), 50);
+	}
+	
+	
 	
 	
 	
