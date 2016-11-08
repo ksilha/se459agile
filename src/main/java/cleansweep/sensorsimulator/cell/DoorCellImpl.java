b56b11a51c19002581c78a8a6a98267fd35afd77
Kevin <ksilha@gmail.com>
2016-11-08 11:32:17 -0600

    Added dirt sensing funtionality to DoorCellImpl

--- src/main/java/cleansweep/sensorsimulator/cell/DoorCellImpl.java
+++ src/main/java/cleansweep/sensorsimulator/cell/DoorCellImpl.java
@@ -1,21 +1,30 @@
 package cleansweep.sensorsimulator.cell;
 
+import java.util.Random;
+
 import cleansweep.sensorcontroller.ControllerFacade.FloorType;
 
 public class DoorCellImpl implements Cell  
 {
 	Boolean obstruction;
 	private static FloorType floorType = FloorType.BARE_FLOOR;
+	private int dirtLevel;
 	
 	public DoorCellImpl(Boolean isClosed)
 	{
 		obstruction = isClosed;
+		
+		Random rand = new Random();
+		dirtLevel = rand.nextInt((5 - 1) + 1) + 1;
 	}
 	
 	//default construction, door closed by default
 	public DoorCellImpl()	
 	{
 		obstruction = false;
+		
+		Random rand = new Random();
+		dirtLevel = rand.nextInt((5 - 1) + 1) + 1;
 	}
 	
 	public Boolean isObstruction()
@@ -28,12 +37,15 @@ public class DoorCellImpl implements Cell
 	}
 
 	public boolean senseDirt() {
-		// TODO Auto-generated method stub
+		if (dirtLevel > 0)
+			return true;
+			
 		return false;
 	}
 
 	public void clean() {
-		// TODO Auto-generated method stub
+		if (dirtLevel > 0)
+			dirtLevel--;
 		
 	}
 
